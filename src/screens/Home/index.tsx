91ffd4ee398ec94781a4226b219276bc4267aaa6
RaphaelSR2 <globalsys.raphael@wine.com.br>
2021-12-14 21:12:10 -0300

    Improvement - BackButton Component on CarDetails Screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,7 +4,7 @@ import { RFValue } from "react-native-responsive-fontsize";
 import Logo from "../../assets/logo.svg";
 import { Car } from "../../components/Car";
 
-import { Container, Header, TotalCars, HeaderContent } from "./styles";
+import { Container, Header, TotalCars, HeaderContent, CarList } from "./styles";
 
 export const Home = () => {
   const carData = {
@@ -12,20 +12,11 @@ export const Home = () => {
     name: "RS 5 Coupé",
     rent: {
       period: "AO DIA",
-      price: 120
+      price: 120,
     },
-    thumbnail: "https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png"
-}
-
-const carDataTwo = {
-  brand: "porsche",
-  name: "Porsche 911",
-  rent: {
-    period: "AO DIA",
-    price: 340
-  },
-  thumbnail: "https://e7.pngegg.com/pngimages/622/224/png-clipart-porsche-911-gt3-2017-porsche-911-geneva-motor-show-car-white-porsche-911-r-car-compact-car-car.png"
-}
+    thumbnail:
+      "https://png.monster/wp-content/uploads/2020/11/2018-audi-rs5-4wd-coupe-angular-front-5039562b.png",
+  };
   return (
     <Container>
       <StatusBar
@@ -39,9 +30,11 @@ const carDataTwo = {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
-
-      <Car data={carData} />
-      <Car data={carDataTwo} />
+      <CarList
+        data={[1, 2, 3 ,4, 5, 6, 7, 8, 9, 10, 11, 12]}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      ></CarList>
     </Container>
   );
 };
