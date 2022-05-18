18a3aa2c8576c3d5d1b9a886d9630333d5795f34
RaphaelSR2 <globalsys.raphael@wine.com.br>
2022-05-18 00:47:47 -0300

    feat: listing cars in mycars screen

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,11 +1,36 @@
 import React, { useEffect, useState } from "react";
+import { StatusBar, FlatList } from "react-native";
+import { BackButton } from "../../components/BackButton";
+import { Car } from "../../components/Car";
+import { useTheme } from "styled-components";
 import { CarDTO } from "../../dtos/CarDTO";
 import api from "../../services/api";
-import { Container } from "./styles";
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+} from "./styles";
+
+interface CarProps{
+  id: string;
+  user_id: string;
+  car: CarDTO;
+}
 
 export function MyCars() {
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [cars, setCars] = useState<CarProps[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+
+  const theme = useTheme();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
 
   useEffect(() => {
     async function fetchCars() {
@@ -22,9 +47,35 @@ export function MyCars() {
   }, []);
   return (
     <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton onPress={handleGoBack} color={theme.colors.shape} />
 
+        <Title>
+          Seus agendamentos,{"\n"}
+          estão aqui.
+        </Title>
+        <SubTitle>Conforto, segurança e praticidade.</SubTitle>
+      </Header>
+
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
+        </Appointments>
+        <FlatList
+          data={cars}
+          keyExtractor={(item) => item.id}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => <Car data={item.car} />}
+        />
+      </Content>
     </Container>
-  )
+  );
 }
 
 export default MyCars;
