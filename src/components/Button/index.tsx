19100efd0e226465a6e77911c88a452eb4593b04
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2022-07-04 13:44:12 -0300

    feat: set loading state in button props and active indicator

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ActivityIndicator } from "react-native";
 import { RectButtonProps } from "react-native-gesture-handler";
 import { useTheme } from "styled-components";
 
@@ -9,6 +10,7 @@ interface ButtonProps extends RectButtonProps {
   color?: string;
   onPress: () => void;
   enabled?: boolean;
+  loading?: boolean;
 }
 
 export function Button({
@@ -16,6 +18,7 @@ export function Button({
   color,
   onPress,
   enabled = true,
+  loading = false,
   ...rest
 }: ButtonProps) {
   const theme = useTheme();
@@ -25,9 +28,13 @@ export function Button({
       color={color ? color : theme.colors.main}
       onPress={onPress}
       enabled={enabled}
-      style={{ opacity: enabled ? 1 : 0.5 }}
+      style={{ opacity: enabled === false || loading == true ? 0.5 : 1 }}
     >
-      <Title>{title}</Title>
+      {loading ? (
+        <ActivityIndicator color={theme.colors.shape} />
+      ) : (
+        <Title>{title}</Title>
+      )}
     </Container>
   );
 }
--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -52,6 +52,7 @@ interface UnavailableDatesProps {
 }
 
 export function SchedulingDetails() {
+  const [loading, SetLoading] = useState(false);
   const [rentalPeriod, setRentalPeriod] = useState<RentalPeriod>(
     {} as RentalPeriod
   );
@@ -62,6 +63,7 @@ export function SchedulingDetails() {
   const rentTotal = Number(dates.length * car.rent.price);
 
   async function handleConfirmRental() {
+    SetLoading(true);
     const schedulesByCar = await api.get(`/schedules_bycars/${car.id}`);
     const dataUnavailableDates = schedulesByCar.data as UnavailableDatesProps;
     const unavailableDates = dataUnavailableDates.unavailable_dates;
@@ -84,7 +86,10 @@ export function SchedulingDetails() {
         unavailable_dates,
       })
       .then(() => navigation.navigate("SchedulingComplete"))
-      .catch(() => Alert.alert("Não foi possível confirmar o agendamento."));
+      .catch(() => {
+        SetLoading(false);
+        Alert.alert("Não foi possível confirmar o agendamento.");
+      });
   }
 
   function handleGoBack() {
@@ -166,6 +171,8 @@ export function SchedulingDetails() {
           title="Alugar agora"
           color={theme.colors.success}
           onPress={handleConfirmRental}
+          enabled={!loading}
+          loading={loading}
         />
       </Footer>
     </Container>
