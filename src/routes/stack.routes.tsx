38a1378983b315e830094dbd99b0e35454ea35ac
Raphael Souza Rocha <raphaelrochabcc@gmail.com>
2022-07-16 18:24:49 -0300

    feat: splash screen and reanimated

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -6,17 +6,19 @@ import { Scheduling } from "../screens/Scheduling";
 import { SchedulingComplete } from "../screens/SchedulingComplete";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { MyCars } from "../screens/MyCars";
+import { Splash } from "../screens/Splash";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function StackRoutes() {
   return (
     <Navigator
-      initialRouteName="Home"
+      initialRouteName="Splash"
       screenOptions={{
         headerShown: false,
       }}
     >
+      <Screen name="Splash" component={Splash} />
       <Screen name="Home" component={Home} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Scheduling" component={Scheduling} />
